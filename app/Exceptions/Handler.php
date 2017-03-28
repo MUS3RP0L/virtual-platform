d84547631e1e8be1040ea7819f1dcd08b66553dd
erickguis <aleguis@icloud.com>
2017-03-27 22:36:25 -0400

    changed namespace

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Exceptions;
+namespace Muserpol\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Muserpol\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use Muserpol\User;
+use Muserpol\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
