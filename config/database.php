420ecb16a4247f9e41cbeef9ecee255cfd4c3256
erickguis <aleguis@icloud.com>
2017-03-28 09:10:33 -0400

    bug fixes

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE_TESTS', 'forge1'),
+            'database' => env('DB_DATABASE_TESTS', 'forge'),
             'username' => env('PGUSER', env('DB_USERNAME', 'forge')),
             'password' => env('PGPASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8',
